257fe958be7ad67277a94e7f524b6ab28b01ee55
Rohit Ghosh <rohitghosh101@gmail.com>
2024-06-17 14:55:14 -0400

    updated dependencies

--- setup.py
+++ setup.py
@@ -4,44 +4,51 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [
+    "dill==0.3.8",
+    "icecream==2.1.3",
+    "pandas==2.2.2",
+    "setuptools==69.1.1",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Rohit Ghosh",
-    author_email='rohit.ghosh@yale.edu',
-    python_requires='>=3.6',
+    author_email="rohit.ghosh@yale.edu",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Tools for the Ghosh.",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
     package_data={
-        'ghoshtools': ['resources/*', 'work/*'],
+        "ghoshtools": ["resources/*", "work/*"],
     },
-    keywords='ghoshtools',
-    name='ghoshtools',
-    packages=find_packages(include=['ghoshtools', 'ghoshtools.*']),
-    test_suite='tests',
+    keywords="ghoshtools",
+    name="ghoshtools",
+    packages=find_packages(include=["ghoshtools", "ghoshtools.*"]),
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/rghosh670/ghoshtools',
-    version='0.1.0',
+    url="https://github.com/rghosh670/ghoshtools",
+    version="0.1.0",
     zip_safe=False,
 )
